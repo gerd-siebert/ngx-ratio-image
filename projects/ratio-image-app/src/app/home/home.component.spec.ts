62ef9c9a782042a5bb6cb1f5098c7c56c2147f29
gerd <gerd.siebert@gmail.com>
2023-06-21 13:03:20 +0200

    fix component test

--- projects/ratio-image-app/src/app/home/home.component.spec.ts
+++ projects/ratio-image-app/src/app/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { RatioImageModule } from 'ngxRatioImage';
 
 describe('HomeComponent', () => {
 	let component: HomeComponent;
@@ -8,7 +10,8 @@ describe('HomeComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			declarations: [HomeComponent]
+			declarations: [HomeComponent],
+			imports: [HomeRoutingModule, RatioImageModule]
 		});
 		fixture = TestBed.createComponent(HomeComponent);
 		component = fixture.componentInstance;
--- projects/ratio-image-app/src/app/interactive/interactive.component.spec.ts
+++ projects/ratio-image-app/src/app/interactive/interactive.component.spec.ts
@@ -1,6 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InteractiveComponent } from './interactive.component';
+import { InteractiveRoutingModule } from './interactive-routing.module';
+import { RatioImageModule } from '../../../../ngx-ratio-image/src/lib/ratio-image.module';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { NgForOf } from '@angular/common';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 describe('InteractiveComponent', () => {
 	let component: InteractiveComponent;
@@ -8,7 +18,19 @@ describe('InteractiveComponent', () => {
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({
-			declarations: [InteractiveComponent]
+			declarations: [InteractiveComponent],
+			imports: [
+				InteractiveRoutingModule,
+				RatioImageModule,
+				MatFormFieldModule,
+				MatInputModule,
+				FormsModule,
+				MatOptionModule,
+				MatSelectModule,
+				NgForOf,
+				MatGridListModule
+			],
+			providers: [provideAnimations()]
 		});
 		fixture = TestBed.createComponent(InteractiveComponent);
 		component = fixture.componentInstance;
